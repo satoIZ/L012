83cae60ead661f8d8ee01befd27e45c547634b90
WOCae <t-satoIZ@outlook.jp>
2024-02-12 21:19:38 +0900

    Update main.gd

--- main.gd
+++ main.gd
@@ -6,25 +6,27 @@ var Enemy = preload("res://enemy.tscn")
 
 func _process(delta):
 
-	var cnt = 0 #enemyのカウント
-	
-	#-----ツリーのノード名を検索
-	for child in get_children():
-		print(child.name)
-		if "Enemy" in child.name:
-			cnt += 1
-			#print("Enemy" )
-	print("個数：" + str(cnt))
-	if cnt == 0:
-		#print("NoEnemy" )
-		pass
-	#----------------------
+	#var cnt = 0 #enemyのカウント
+	#
+	##-----ツリーのノード名を検索
+	#for child in get_children():
+		##print(child.name)
+		#if "Enemy" in child.name:
+			#cnt += 1
+			##print("Enemy" + str(cnt))
+	#print("個数：" + str(cnt))
+	#if cnt == 0:
+		##print("NoEnemy" )
+		#pass
+	##----------------------
 	
 	#----エネミーが0になった場合にスポーンする
-	if cnt == 0:
-	#if Common.countEne == 0:
+	#if cnt == 0:
+	if Common.emCount == 0:
 		for i in range(3):
 			var Enemy = Enemy.instantiate()
 			Enemy.setPos(position.x + i*xPos+100, position.y + yPos)
 			Enemy.name = "Enemy" + str(i)
 			self.add_child(Enemy)
+			
+			Common.emCount += 1
