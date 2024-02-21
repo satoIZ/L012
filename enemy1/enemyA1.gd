2a65ee71b62aab807baf8b8cd3c3291a04d6c45f
WOCae <t-satoIZ@outlook.jp>
2024-02-21 13:01:54 +0900

    Update enemyA1.gd

--- enemy1/enemyA1.gd
+++ enemy1/enemyA1.gd
@@ -3,6 +3,8 @@ extends Node2D
 var xPos = 250
 var yPos = 100
 var Enemy = preload("res://enemy1/enemy1.tscn")
+var Item = preload("res://Item/item.tscn")
+var ItemFlag:bool = false
 
 func _process(delta):
 
@@ -28,5 +30,20 @@ func _process(delta):
 			Enemy.setPos(position.x + i*xPos+100, position.y + yPos)
 			Enemy.name = "Enemy" + str(i)
 			self.add_child(Enemy)
-			
+			ItemFlag = true
 			#Common.emCount += 1
+
+	#----エネミーが0になった場合アイテムをスポーンする
+	if ItemFlag == true:
+	#if Common.emCount == 0:
+		for i in range(1):
+			var Item = Item.instantiate()
+			var item_x = randi_range(0, 10)
+			print(item_x*100)
+			Item.setPos( item_x*100, position.y + yPos+200)
+			Item.name = "Item" + str(i)
+			self.add_child(Item)
+	
+		ItemFlag = false
+	
+	
