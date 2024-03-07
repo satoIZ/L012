3c95203d6365c4322d2d6e34ebf83ba1a87b0af7
WOCae <t-satoIZ@outlook.jp>
2024-03-07 10:54:29 +0900

    Update main.gd

--- main.gd
+++ main.gd
@@ -4,6 +4,11 @@ var xPos = 250
 var yPos = 100
 var Enemy = preload("res://enemy.tscn")
 
+func _ready():
+	
+	Common.bullets = $Bullets
+	
+	
 func _process(delta):
 
 	#var cnt = 0 #enemyのカウント
@@ -30,3 +35,32 @@ func _process(delta):
 			self.add_child(Enemy)
 			
 			Common.emCount += 1
+
+			#count2 += 1
+			#if count2 == 1:
+			
+			#for ii in range(3):			
+				#await get_tree().create_timer(1).timeout
+				#addBullet(position.x + i*xPos+100, position.y + yPos)
+				
+				#count2 = 0
+#enemyから弾を射出
+#弾の設定
+#var Bullet   = preload("res://bullet.tscn")
+#var count2:int
+#var degcount:float
+#var em_X:float
+#var em_Y:float
+
+#func addBullet(x,y):
+#
+	##count2 += 1
+	##if count2 == 1:
+		#var bullet = Bullet.instantiate()
+		#bullet.start(-45,  100)
+		#bullet.setPos(x,y)
+		## ルートノードを取得
+		#add_child(bullet)
+		#await get_tree().create_timer(1).timeout
+		#count2 = 0
+		
