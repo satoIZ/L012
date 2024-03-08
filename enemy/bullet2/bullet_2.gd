aff8bfce8186d1b8fbbf63a42ae5e7544d1c6ff1
WOCae <t-satoIZ@outlook.jp>
2024-03-08 12:28:19 +0900

    Create bullet_2.gd

---
+++ enemy/bullet2/bullet_2.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+class_name Bullet2
+
+# 移動方向
+var velocity = Vector2()
+
+# 開始
+func setPos(x, y):
+	position = Vector2(x, y)
+	
+# 開始
+func start(deg, speed):
+	velocity.x = speed * cos(deg_to_rad(deg))
+	velocity.y = speed * -sin(deg_to_rad(deg))
+
+func _physics_process(delta):
+	position += velocity * delta
+	
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()	
+	#pass # Replace with function body.
+
+
